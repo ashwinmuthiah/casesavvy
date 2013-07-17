c2f4948b9b50ff1a8cc0ab71c414b9c73bbd1a30
Ashwin Muthiah <muthiah.ashwin2015@gmail.com>
2013-07-17 16:37:49 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Welcome to CaseSavvy where you can get artistic iPhone cases.
+
+by Ashwin Muthiah 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Casesavvy::Application.config.secret_token = '79239ed6d535a1c36a95c84bc2baf36e09a0e9947d407758fec6cb5117c5b5dc332cab1a0caa6b149fa19de5116fbed0b537b12a2bb4ef019abc2cbd23aba3f7'
