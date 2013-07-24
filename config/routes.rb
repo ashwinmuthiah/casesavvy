8cab4e6542d7738339ae80f350e92631be78852f
Ashwin Muthiah <muthiah.ashwin2015@gmail.com>
2013-07-24 15:19:19 -0400

    Change to Boylan Art

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Casesavvy::Application.routes.draw do
   
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about2'
   root :to => 'pages#home'
 
   # The priority is based upon order of creation:
